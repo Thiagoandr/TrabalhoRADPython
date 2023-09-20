10f54086cc7e4c01cefd55d21003cda98f877baf
Mulinatti <gabrielmulinari2002@gmail.com>
2023-09-20 15:18:10 -0300

    melhor visualização da interface

--- cadastro_de_alunos/matricula.py
+++ cadastro_de_alunos/matricula.py
@@ -2,11 +2,15 @@ from opcoes import mostrar_opcoes, cadastrar_aluno, mostrar_alunos, buscar_aluno
 import os
 import time
 
+from colorama import Fore, Back, Style, init
+
+init(autoreset=True)
+
 while True:
     try:
         os.system("cls")
         mostrar_opcoes()
-        opcao = int(input("Escolha uma opção: "))
+        opcao = int(input(Fore.YELLOW + "Escolha uma opção: " + Style.RESET_ALL))
         os.system("cls")
 
         if opcao == 1:
@@ -16,12 +20,13 @@ while True:
         elif opcao == 3:
             buscar_aluno()
         elif opcao == 4:
-            print("Encerrando programa")
+            print(Fore.BLUE + "Encerrando programa" + Style.RESET_ALL)
             break
         else:
-            print("Digite uma opção válida")
+            print(Fore.RED + "Digite uma opção válida" + Style.RESET_ALL)
+            time.sleep(2)
     except ValueError:
         os.system("cls")
-        print("Digite corretamente o que foi pedido")
+        print(Fore.RED + "Digite corretamente o que foi pedido" + Style.RESET_ALL)
         time.sleep(3)
 
--- cadastro_de_alunos/opcoes.py
+++ cadastro_de_alunos/opcoes.py
@@ -1,24 +1,28 @@
 import time
+from colorama import Fore, Back, Style, init
+
+init(autoreset=True)
 
 nome_arquivo = "cadastro_de_alunos/alunos.txt"
 
 def mostrar_opcoes():
-    print("ESCOLHA UMA DAS OPÇÕES\n")
+    print(Fore.YELLOW + "ESCOLHA UMA DAS OPÇÕES\n" + Style.RESET_ALL)
     print("1 - Cadastrar aluno")
     print("2 - Listar alunos")
     print("3 - Buscar aluno")
-    print("4 - Sair")
+    print("4 - Sair \n")
 
 
 def cadastrar_aluno():
+    print(Fore.YELLOW + "Informe os dados para cadastrar o aluno\n" + Style.RESET_ALL)
     nome = input("Informe o nome do aluno: ")
     curso = input("Informe o curso do aluno: ")
     email = input("Informe o E-Mail do aluno: ")
 
     for caractere in nome:
         if caractere.isdigit():
-            print("O Campo nome possui números.")
-            print("O Cadastro não foi efetuado.")
+            print(Fore.RED + "O Campo nome não pode possuir números.\n" + Style.RESET_ALL)
+            print(Fore.BLUE + "O Cadastro não foi efetuado." + Style.RESET_ALL)
             time.sleep(3)
             return
 
@@ -27,6 +31,8 @@ def cadastrar_aluno():
     arquivo = open(nome_arquivo, "a")
     arquivo.write(aluno)
     arquivo.close()
+    print(Fore.GREEN + "Aluno cadastrado com sucesso!" + Style.RESET_ALL)
+    time.sleep(3)
 
 def mostrar_alunos():
     arquivo = open(nome_arquivo, "r")
@@ -39,7 +45,7 @@ def mostrar_alunos():
 
 
 def buscar_aluno():
-    busca = input("Informe o aluno a ser buscado: ")
+    busca = input(Fore.YELLOW + "Informe o aluno a ser buscado: " + Style.RESET_ALL)
 
     arquivo = open(nome_arquivo, "r")
     alunos = arquivo.readlines()
@@ -48,13 +54,13 @@ def buscar_aluno():
 
     for aluno in alunos:
         if busca in aluno:
-            print("Aluno encontrado\n")
+            print(Fore.BLUE + "Aluno encontrado\n" + Style.RESET_ALL)
             print(aluno)
             encontrou = True
         
 
     if encontrou == False:
-        print("Aluno não encontrado")
+        print(Fore.BLUE + "Aluno não encontrado" + Style.RESET_ALL)
 
 
     time.sleep(3)
